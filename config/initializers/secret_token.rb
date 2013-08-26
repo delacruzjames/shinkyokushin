dd00c9b3f751d23279b5678e2b0c9befa171ba83
James Martin Dela Cruz <james@bazinga.com.ph>
2013-08-26 10:15:35 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shinkyokushin::Application.config.secret_token = '1c0a3e18427d30900f56cf0f735f154d8a8a25beeec1caba88c7552ebdfa4f233c0b19b35e99fdf93426c8e8f739a457d4179ec49e8248d751a6b287a9448329'
