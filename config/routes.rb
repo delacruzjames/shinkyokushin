fbf7f8ef14cd44f505e1d57fb7b078f6f1a0cb5f
James Martin Dela Cruz <james@bazinga.com.ph>
2013-09-01 13:00:13 +0800

    check all specs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Shinkyokushin::Application.routes.draw do
 
   resources :dojos, :only => [:show]
   resources :instructors, :only => [:show]
-  devise_for :users
+  devise_for :users do
+  	match '/registration', :to => "devise/registrations#new"
+  end
   root :to => 'pages#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
--- spec/routing/pages_controller_routing_spec.rb
+++ spec/routing/pages_controller_routing_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe "PagesControllerRouting" do
-	it { get("/").should route_to("pages#index")}
+	context "correct proper routing" do
+		it { get("/").should route_to("pages#index")}
+	end
 end
