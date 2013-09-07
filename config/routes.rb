0c7d8f2897431ccc75fe7941696a03c2120fe209
James Martin Dela Cruz <james@bazinga.com.ph>
2013-09-08 07:35:40 +0800

    fix users resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Shinkyokushin::Application.routes.draw do
 
-  get "users/index"
-
   resources :dojos, :only => [:show, :index]
   resources :instructors, :only => [:show, :index]
-  resources :users, :only => [:show, :index]
-  devise_for :users do
-  	match '/registration', :to => "devise/registrations#new"
-  	match '/login', :to => "devise/sessions#new"
-  end
+  # resources :users, :only => [:show, :index]
+  devise_for :users 
   root :to => 'pages#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
