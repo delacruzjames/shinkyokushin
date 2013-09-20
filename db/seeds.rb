da5ed009266e7dd0fc413f520362989d73c91904
James Martin Dela Cruz <james@bazinga.com.ph>
2013-09-20 22:27:56 +0800

    add default image avatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-instructors = ["richard", "james", "alex", "marjon", "janedee", "ram"]
+instructors = ["rose", "james", "charlie", "carl", "delacruz"]
 students = ["anne", "martin", "david", "solomon", "bea", "blessie", "marie"]
 instructors.each do |name|
-	Instructor.create!(:email => "#{name}@shinkyokushin.com.ph", :password => "password", :password_confirmation => "password", :first_name => "Sensie #{name.capitalize}", :last_name => "MyLastName", :avatar_profile => File.open(Rails.root.join("app","assets","images","#{name}.jpg")))
+	Instructor.create!(:email => "#{name}@shinkyokushin.com.ph", :password => "password", :password_confirmation => "password", :first_name => "Sensie #{name.capitalize}", :last_name => "MyLastName", :avatar_profile => File.open(Rails.root.join("app","assets","images","shinkyokushin_default_image.jpg")))
 end
 
 puts "Creating instructors"
@@ -14,7 +14,7 @@ end
 puts "Creating Dojos"
 
 students.each do |student|
-	Member.create!(:email => "#{student}@shinkyokushin.com.ph", :password => "password", :password_confirmation => "password", :dojo_id => rand(1..4), :first_name => "Sempai #{student.capitalize}", :last_name => "MyLastName", :avatar_profile => File.open(Rails.root.join("app","assets","images","#{student}.jpg")))
+	Member.create!(:email => "#{student}@shinkyokushin.com.ph", :password => "password", :password_confirmation => "password", :dojo_id => rand(1..4), :first_name => "Sempai #{student.capitalize}", :last_name => "MyLastName", :avatar_profile => File.open(Rails.root.join("app","assets","images","shinkyokushin_default_image.jpg")))
 end
 
 puts "Creating Students.."
