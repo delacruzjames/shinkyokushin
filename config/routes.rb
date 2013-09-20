5e82213701f7b4baa28608dadb255bc34f218831
James Martin Dela Cruz <james@bazinga.com.ph>
2013-09-20 22:58:55 +0800

    fix membershow

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Shinkyokushin::Application.routes.draw do
 
-  get "members/index"
-
   resources :dojos, :only => [:show, :index]
   resources :instructors, :only => [:show, :index]
   resources :members, :only => [:show, :index]
   devise_for :users do
   end 
   root :to => 'pages#index'
-  get "/member" => 'member#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 end
