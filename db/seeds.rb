f71fe8ed84b2a11aa7fa5f0ca03bae1943b7a48b
James Martin Dela Cruz <james@bazinga.com.ph>
2013-09-24 23:05:54 +0800

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
 
 dojos = ["manila", "qc", "caloocan", "makati", "pasay"]
 
+instructor = Instructor.create!(:email => 'instructor@shinkyokushin.com.ph', :password => 'password', :password_confirmation => 'password')
+puts "Creating Instructor... "
+puts "#{instructor.email}.."
+
 dojos.each do |dojo|
-	instructor = Instructor.create!(:email => 'instructor@shinkyokushin.com.ph', :password => 'password', :password_confirmation => 'password')
-	instructor.dojos.create!(:name => '#{dojo}.jpg', :description => '#{dojo}-description'
+	instructor = Instructor.last
+	instructor.dojos.create!(:name => "#{dojo.upcase}", :description => "#{dojo}-description", :avatar => File.open(Rails.root.join("app","assets","images","#{dojo}.jpg")))
 end
+
+puts "Creating Dojos..."
 
 
