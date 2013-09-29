edbf385fcaed65e29acd1b4091c5d2c2e394ce63
Kenneth Lindsey Calamay <git@kennethcalamay.com>
2013-09-29 18:42:22 +0800

    Gets rid of the annoying Devise deprecation warning

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Shinkyokushin::Application.routes.draw do
 
+  root :to => 'pages#index'
+
   match "/dashboard", :to => "pages#instructor_dashboard"
+
   resources :dojos, except: [:edit, :update, :destroy]
   resources :instructors, :only => [:show, :index]
   resources :members, :only => [:show, :index]
-  devise_for :users do
-  end 
-  root :to => 'pages#index'
+
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
+
   ActiveAdmin.routes(self)
 end
