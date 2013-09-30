b7c845d93979a5692d28be855c582d4b9e8ee228
James Martin Dela Cruz <james@bazinga.com.ph>
2013-09-30 16:11:09 +0800

    ignore schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130911120909) do
+ActiveRecord::Schema.define(:version => 20130928160603) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(:version => 20130911120909) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "belts", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "dojos", :force => true do |t|
     t.string   "name"
@@ -102,6 +109,8 @@ ActiveRecord::Schema.define(:version => 20130911120909) do
     t.string   "contact_person_address"
     t.string   "slug"
     t.string   "avatar_profile"
+    t.integer  "belt_id"
+    t.string   "identification_no"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
