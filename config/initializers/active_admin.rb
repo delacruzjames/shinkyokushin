c7f1f2614d9d4248b4dde6183990a955382781d6
James Martin Dela Cruz <james@bazinga.com.ph>
2013-11-10 07:29:52 +0800

    remove comment on active admin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,11 +1,12 @@
 ActiveAdmin.setup do |config|
 
+  config.allow_comments = false
   # == Site Title
   #
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Shinkyokushin"
+  config.site_title = "Shinkyokushin Philippines"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
