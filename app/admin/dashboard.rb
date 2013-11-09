abc9b41d6bf928f1b801b0ce18cc0c373e53c26c
James Martin Dela Cruz <james@bazinga.com.ph>
2013-11-09 14:03:11 +0800

    fix initial registration

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,24 +4,41 @@ ActiveAdmin.register_page "Dashboard" do
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
 
+
     columns do
       column do
-        panel "All Instructors" do
-
-        end
-      end
-      column do
-        panel "All Dojos" do
-          
+        panel "Pending Members" do
+          div :class => "center" do
+            b ""
+          end
         end
       end
     end
 
     columns do
       column do
-        panel "New Students" do
+        panel "Approved Members" do
           div :class => "center" do
             b ""
+          end
+        end
+      end
+    end
+
+    columns do
+      column do
+        panel "Instructors" do
+          table_for Instructor.all.each do |table|
+            table.column("Full Name") {|instructor| instructor.full_name }
+            table.column("Last Login") {|instructor| instructor.last_sign_in_at }
+          end
+        end
+      end
+      column do
+        panel "Dojos" do
+          table_for Dojo.all.each do |table|
+            table.column("Name") {|dojo| dojo.name }
+            table.column("Members") {|dojo| dojo.members.count }
           end
         end
       end
